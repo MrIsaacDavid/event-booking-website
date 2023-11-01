ffc75a3aa8da208fb113a6f4ac335ce4c57339c1
MrIsaacDavid <146441017+MrIsaacDavid@users.noreply.github.com>
2023-11-01 16:01:23 +0300

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -7,31 +7,7 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth =Blueprint('auth',__name__)
 
-@auth.route('/login',methods=['GET', 'POST'])
-def login():
-        if request.method == 'POST':
-            email = request.form.get('email')
-            password = request.form.get('password')
-
-            user = User.query.filter_by(email=email).first()
-            if user:
-               if check_password_hash(user.password, password):
-                 flash('Logged in successfully!', category='success')
-                 login_user(user, remember=True)
-                 return redirect(url_for('views.home'))
-               else:
-                flash('Incorrect password, try again.', category='error')
-            else:
-             flash('Email does not exist.', category='error')
-        return render_template("login.html", user=current_user)
-
-@auth.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('auth.login'))
-
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route('/sign-up', methods=['POST'])
 def sign_up():
     data = request.get_json()
     if data and "email" in data and "firstName" in data and "password" in data:
@@ -46,9 +22,53 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            return jsonify({'message': 'User created successfully!'})
+            return jsonify({'message': 'User created successfully!'}),201
         else:
             return jsonify({'message': 'Email already exists!'})
 
     else:
         return jsonify({"message": "Invalid data!"})
+
+      
+@auth.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if email and password:
+        user = User.query.filter_by(email=email).first()
+
+        if user and check_password_hash(user.password, password)==True:  # Assuming you have a method to check the password in your User model
+            
+            login_user(user)
+            return jsonify({'message': 'Successfully logged in', 'email': email}), 200
+        else:
+            return jsonify({'message': 'Unauthorized user'}), 401
+    else:
+        return jsonify({'message': 'Bad Request - Missing email or password'}), 400
+
+@auth.route('/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)  # Retrieve the user by their ID
+
+    if user:
+        # If the user is found, return their data as JSON response
+        response = {
+            'id': user.id,
+            'email': user.email,
+            'password': user.password
+        }
+        return jsonify(response), 200
+    else:
+        # If user is not found, return a 404 Not Found response
+        return jsonify({'message': 'User not found'}), 404
+    
+@auth.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        users_list.append({'id': user.id, 'email': user.email})
+    return jsonify({'users': users_list})
+
