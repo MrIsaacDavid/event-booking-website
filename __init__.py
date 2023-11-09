ebbd47c385a4418816ce2a5de225cc96d95a9b38
MrIsaacDavid <146441017+MrIsaacDavid@users.noreply.github.com>
2023-11-09 20:46:34 +0300

    Add files via upload
    New updates

---
+++ __init__.py
@@ -0,0 +1,46 @@
+
+#this folder makes the website folder a python package
+from flask import Flask # imports the Flask class.
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+#create a function which be called by main.py to run the application
+def create_app():
+    app=Flask(__name__) #creates a Flask web server.
+
+    app.config['SECRET_KEY']='nsbcsdnmbnasbdnfjsdfjd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views 
+    from .auth import auth
+
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+    
+    from .models import User
+    
+    with app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
+    
---
+++ models.py
@@ -0,0 +1,82 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+import xdrlib
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from datetime import datetime
+
+
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    venue = db.Column(db.String(200), nullable=False)
+    bookings = db.relationship('Booking', backref='event_relation', lazy=True)
+
+    def __init__(self, name, date,venue):
+        self.name = name
+        self.date = date
+        self.venue = venue
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+
+    
+    user= db.relationship('User', backref=db.backref('user_bookings', lazy=True))
+    event = db.relationship('Event', backref=db.backref('event_bookings', lazy=True))
+
+
+    def __init__(self, user_id, event_id):
+        self.user_id = user_id
+        self.event_id = event_id
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    bookings=db.relationship('Booking')
+    is_admin = db.Column(db.Boolean, default=False)
+
+
+      
+    def __init__(self, email, password,first_name):
+        self.email = email
+        self.password = password
+        self.first_name=first_name
+        
+=======
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    
+>>>>>>> ffc75a3aa8da208fb113a6f4ac335ce4c57339c1
+=======
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    
+>>>>>>> ffc75a3aa8da208fb113a6f4ac335ce4c57339c1
