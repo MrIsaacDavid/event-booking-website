0f735613e3de70d5919bd8e48f9ed8df79d41e59
MrIsaacDavid <146441017+MrIsaacDavid@users.noreply.github.com>
2023-11-15 13:29:50 +0300

    Export  models to Mysql Workbench

--- website/__init__.py
+++ website/__init__.py
@@ -4,15 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
+
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "event"
+KEY='nsbcsdnmbnasbdnfjsdfjd1234567890'
 
 #create a function which be called by main.py to run the application
 def create_app():
     app=Flask(__name__) #creates a Flask web server.
 
-    app.config['SECRET_KEY']='nsbcsdnmbnasbdnfjsdfjd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SECRET_KEY'] = KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:*3Wangari4#!@localhost/' + DB_NAME
+    # Replace 'username' and 'password' with your MySQL credentials
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
     from .views import views 
@@ -22,7 +26,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     
-    from .models import User
+    from .models import User,Event, Booking
     
     with app.app_context():
         db.create_all()
@@ -32,14 +36,19 @@ def create_app():
     login_manager.init_app(app)
 
     @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
+    def user_loader(user_id):
+        user = User.query.filter_by(id=user_id).first()
+        if user:
+            return user
+        return None
 
     return app
 
-
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
+    if not path.exists(path.join('website' + DB_NAME)):
+        with app.app_context():
+            db.init_app(app)
+            db.create_all()
         print('Created Database!')
-    
+
+
